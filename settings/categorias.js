1dca62ea6311049fb92f9cef30168901cc14f399
eb1084022023-svg <eb1084022023@unab.edu.sv>
2025-10-27 21:53:09 +0000

    settings

---
+++ settings/categorias.js
@@ -0,0 +1,20 @@
+const categoriasDeJuegos = [
+  "Acción",
+  "Aventura",
+  "RPG (Juegos de Rol)",
+  "Estrategia",
+  "Simulación",
+  "Deportes",
+  "Carreras",
+  "Lucha",
+  "Plataformas",
+  "Puzles",
+  "Musicales",
+  "Arcade",
+  "Survival Horror",
+  "MMORPG",
+  "Shooter (Disparos)",
+  "Battle Royale"
+];
+
+console.log(categoriasDeJuegos);
